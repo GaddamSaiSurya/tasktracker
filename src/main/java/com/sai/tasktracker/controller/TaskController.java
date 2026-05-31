d2678aa100371b6ccfb6c17642da38ad9ba5b2f1
Surya <surya104441@gmail.com>
2026-05-31 16:12:38 +0530

    add task controller

---
+++ src/main/java/com/sai/tasktracker/controller/TaskController.java
@@ -0,0 +1,25 @@
+package com.sai.tasktracker.controller;
+
+import com.sai.tasktracker.entity.Task;
+import com.sai.tasktracker.service.TaskService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks(){
+        return taskService.getAllTasks();
+    }
+}
