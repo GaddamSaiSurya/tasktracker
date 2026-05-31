7c53b003ceaa3f080e0e52f688a81884c2912d64
Surya <surya104441@gmail.com>
2026-05-31 16:04:46 +0530

    add task service

---
+++ src/main/java/com/sai/tasktracker/service/TaskService.java
@@ -0,0 +1,20 @@
+package com.sai.tasktracker.service;
+
+import com.sai.tasktracker.entity.Task;
+import com.sai.tasktracker.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
+    }
+}
